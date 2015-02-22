c49673ce6dd2af260c12b01a5b9200fbbb268d5a
Lyza Danger Gardner <lyza@lyza.com>
2015-02-22 13:05:58 -0800

    Watching for changes to all imported CSS files
    Was previously just watching styles.css which would miss changes to component CSS that gets imported.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,5 +6,5 @@ var config   = require('../config');
 gulp.task('watch', ['browserSync'], function(callback) {
   // This will change (task name)
   gulp.watch(config.protoHTML.src, ['proto-html']);
-  gulp.watch(config.css.src, ['css']);
+  gulp.watch(config.css.importDir + '/**/*', ['css']);
 });
