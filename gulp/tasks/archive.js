a8a24e8c64eb616d01f6597d582a7be6e221c0c6
Lyza Danger Gardner <lyza@lyza.com>
2015-03-05 15:12:55 -0800

    Hackneyed archive task for now.

--- gulp/tasks/archive.js
+++ gulp/tasks/archive.js
@@ -3,11 +3,26 @@
 
 var gulp    = require('gulp');
 var config  = require('../config').archive;
+var fs      = require('fs');
 var data    = require('gulp-data');
 var fakeSrc = require('vinyl-fs-fake');
 var template = require('../plugins/template');
 
+/* @TODO: Move me somewhere utilitarian */
+var readDrafts = function() {
+  // Right now this is super flat and basic
+  var drafts = fs.readdirSync(config.drafts.dest);
+  var links = [];
+  drafts.forEach(function(filename) {
+    if (filename.indexOf('index.html') < 0) {
+      links.push(filename);
+    }
+  });
+  return links;
+};
+
 gulp.task('archive', ['drafts'], function() {
+  var links = readDrafts();
   return fakeSrc.src([{
     contents: new Buffer(''),
     cwd: '',
@@ -15,8 +30,11 @@ gulp.task('archive', ['drafts'], function() {
     path: 'index.html'
   }])
     .pipe(data(function(file) {
+      // @TODO Build context/data elsewhere
       return {
-        template: 'archive'
+        template: 'archive',
+        archiveName: 'Drafts',
+        draftLinks: links
       };
     }))
     .pipe(template())
