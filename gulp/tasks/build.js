2ef2659fe2efb97bf89b9dea9bf94743c95085eb
Lyza Danger Gardner <lyza@lyza.com>
2015-07-05 18:37:30 -0700

    Clean up gulp tasks

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,7 +2,7 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
-gulp.task('build', ['clean'], function (done) {
+gulp.task('build', ['clean', 'publish', 'unpublish'], function (done) {
   var tasks = [
     'assets',
     'content',
