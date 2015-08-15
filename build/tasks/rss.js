343bf2588f9cef28f323a11d9b4b496b8b975e68
Lyza Danger Gardner <lyza@lyza.com>
2015-08-14 19:19:45 -0700

    Fix shit per Bryan's feedback

--- build/tasks/rss.js
+++ build/tasks/rss.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var gulp   = require('gulp');
-var rss    = require('../utils/rss');
+var gulp = require('gulp');
+var rss  = require('../utils/rss');
 
-gulp.task('rss', function(cb) {
-  rss(cb);
-});
+gulp.task('rss', rss);
