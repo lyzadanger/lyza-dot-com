9ffc7cadad9716800c4c8fc9bf8297f9413eb7ba
Lyza Danger Gardner <lyza@lyza.com>
2015-11-30 10:43:08 -0800

    Define default task in gulpfile instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var requireDir = require('require-dir');
+var gulp       = require('gulp');
 
 // Require all tasks in build/tasks, including subfolders
 requireDir('./build/tasks', { recurse: true });
+
+gulp.task('default', ['build', 'watch']);
