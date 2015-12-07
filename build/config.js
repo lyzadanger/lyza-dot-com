d9177ff6ebc38c89a646b8131c6a43a7e39d7284
Lyza Danger Gardner <lyza@lyza.com>
2015-12-07 13:49:30 -0800

    Align stuff

--- build/config.js
+++ build/config.js
@@ -2,23 +2,23 @@
 var highlight = require('highlight.js');
 
 var dest = './dist',
-  src  = './src',
-  dirs = {
-    data: src + '/data',
-    drafts: src + '/content/drafts',
-    js: src + '/javascript',
-    pages: src + '/content/pages',
-    posts: src + '/content/posts',
-    scripts: src + '/javascript',
-    styles: src + '/styles',
+  src    = './src',
+  dirs   = {
+    data     : src + '/data',
+    drafts   : src + '/content/drafts',
+    js       : src + '/javascript',
+    pages    : src + '/content/pages',
+    posts    : src + '/content/posts',
+    scripts  : src + '/javascript',
+    styles   : src + '/styles',
     templates: src + '/templates'
   },
-  srcs = {
-    drafts: dirs.drafts + '/*/**/*.md',
-    js: dirs.js + '/**/*.js',
-    pages: dirs.pages + '/**/*.md',
-    posts: dirs.posts + '/**/*.md',
-    styles: dirs.styles + '/styles.css',
+  srcs   = {
+    drafts   : dirs.drafts + '/*/**/*.md',
+    js       : dirs.js + '/**/*.js',
+    pages    : dirs.pages + '/**/*.md',
+    posts    : dirs.posts + '/**/*.md',
+    styles   : dirs.styles + '/styles.css',
     templates: dirs.templates + '/**/*.hbs'
   };
 
@@ -34,24 +34,24 @@ module.exports = {
     dest: dest
   },
   blog: {
-    author: 'Lyza Danger Gardner',
-    dataDir: dirs.data,
+    author           : 'Lyza Danger Gardner',
+    dataDir          : dirs.data,
     dateDisplayFormat: 'MMMM D, YYYY', // http://momentjs.com/docs/#/displaying/
-    description: 'Developer and Person',
-    domain: 'http://www.lyza.com',
-    feedDir: dest + '/feeds/',
-    language: 'en-US',
-    permalinkPattern: 'YYYY/MM/DD',
-    postFileName: 'index',
-    postExtension: '.md',
-    pageDir: dirs.pages,
-    postDir: dirs.posts,
-    title: 'Lyza Danger Gardner',
-    urlBase: '/'
+    description      : 'Developer and Person',
+    domain           : 'http://www.lyza.com',
+    feedDir          : dest + '/feeds/',
+    language         : 'en-US',
+    permalinkPattern : 'YYYY/MM/DD',
+    postFileName     : 'index',
+    postExtension    : '.md',
+    pageDir          : dirs.pages,
+    postDir          : dirs.posts,
+    title            : 'Lyza Danger Gardner',
+    urlBase          : '/'
   },
   browserSync: {
-    open: false,
-    server: {
+    open     : false,
+    server   : {
       baseDir: dest
     }
   },
@@ -60,8 +60,8 @@ module.exports = {
   },
   css: {
     importDir: dirs.styles,
-    src: srcs.styles,
-    dest: dest + '/css'
+    src      : srcs.styles,
+    dest     : dest + '/css'
   },
   content: {
     src: [ srcs.pages, srcs.posts ],
@@ -77,30 +77,30 @@ module.exports = {
     src: srcs.js
   },
   marked: { // Shared by several tasks
-    gfm: true,
-    highlight: function(code) {
+    gfm        : true,
+    highlight  : function(code) {
       return highlight.highlightAuto(code).value;
     },
     smartypants: true
   },
   markedLite: {
-    gfm: true,
+    gfm        : true,
     smartypants: true
   },
   publish: {
-    dest: dirs.posts,
+    dest  : dirs.posts,
     drafts: dirs.drafts,
-    prune: dirs.drafts,
-    src: srcs.drafts
+    prune : dirs.drafts,
+    src   : srcs.drafts
   },
   unpublish: {
-    src: srcs.posts,
+    src  : srcs.posts,
     prune: dirs.posts,
-    dest: dirs.drafts
+    dest : dirs.drafts
   },
   templates: {
-    partialDir: dirs.templates + '/partials',
+    partialDir : dirs.templates + '/partials',
     templateDir: dirs.templates,
-    src : srcs.templates
+    src        : srcs.templates
   }
 };
