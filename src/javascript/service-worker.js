f89b85543d37156c91a8f8a4373b1990cb5730d3
Lyza Danger Gardner <lyza@lyza.com>
2015-12-09 17:57:42 -0800

    Registering and clearing caches

--- src/javascript/service-worker.js
+++ src/javascript/service-worker.js
@@ -1,8 +1,38 @@
+/* global self, caches */
 'use strict';
 
 (function () {
-  var version = 'hashMe',
-    caches = {};
+  var version  = '898hashMe199898',
+    preCache   = ['/lyza-2.gif',
+                  '/css/styles.css'],
+    cacheNames = {};
+
+  var updateStaticCache = function () {
+    return caches.open(cacheNames.static)
+      .then((cache) => cache.addAll(preCache));
+  };
+
+  var clearOldCaches = function () {
+    return caches.keys()
+      .then(     (keys) => Promise.all(keys
+        .filter( (key) => key.indexOf(version) !== 0 )
+        .map(    (deleteKey) => caches.delete(deleteKey) ) )
+    );
+  };
+
   ['static', 'pages', 'images']
-    .forEach((cacheKey) => caches[cacheKey] = `${version}${cacheKey}`);
+    .forEach((cacheKey) => cacheNames[cacheKey] = `${version}${cacheKey}`);
+
+  self.addEventListener('install', (event) => {
+    event.waitUntil(updateStaticCache()
+      .then(() => self.skipWaiting())
+    );
+  });
+
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(clearOldCaches()
+      .then(() => self.clients.claim())
+    );
+  });
+
 }());
