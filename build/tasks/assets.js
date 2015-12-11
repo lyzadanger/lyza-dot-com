eb084ff73196ca310c21b4154db2c321d82b87d4
Lyza Danger Gardner <lyza@lyza.com>
2015-12-11 10:39:09 -0800

    Add back imagemin

--- build/tasks/assets.js
+++ build/tasks/assets.js
@@ -1,10 +1,12 @@
 'use strict';
 var gulp = require('gulp');
 var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
 var config = require('../config').assets;
 
 gulp.task('assets', function() {
   return gulp.src(config.src)
     .pipe(changed(config.dest))
+    .pipe(imagemin())
     .pipe(gulp.dest(config.dest));
 });
