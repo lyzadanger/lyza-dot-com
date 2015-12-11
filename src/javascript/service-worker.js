009a56530ee55da0cbac397c87da4b95eb458508
Lyza Danger Gardner <lyza@lyza.com>
2015-12-10 16:38:43 -0800

    Clean up

--- src/javascript/service-worker.js
+++ src/javascript/service-worker.js
@@ -8,37 +8,40 @@
                   '/site.js',
                   '/offline/',
                   '/'],
+    cachePathPattern = /^\/(20[0-9]{2}|about|blog|css)/,
     cacheNames = {};
-
-  var updateStaticCache = function () {
-    return caches.open(cacheNames.static)
-      .then((cache) => cache.addAll(preCache));
-  };
-
-  var clearOldCaches = function () {
-    return caches.keys()
-      .then((keys) => Promise.all(keys
-        .filter((key) => key.indexOf(version) !== 0)
-        .map((deleteKey) => caches.delete(deleteKey)) )
-    );
-  };
 
   var shouldHandleFetch = function (request) {
     let url = new URL(request.url),
       criteria = {
-        'fromMyOwnServer': url.origin === self.location.origin,
-        'isAGETRequest': request.method === 'GET',
-        'inAValidPath': (function () {
-          var matchesWhitelist =  /^\/(20[0-9]{2}|about|blog|css)/.exec(url.pathname);
-          var matchesPreCache = preCache.filter((path) => path === url.pathname);
-          return matchesWhitelist || matchesPreCache.length;
-        }())
+        'fromMyOwnServer':  url.origin === self.location.origin,
+        'isAGETRequest':    request.method === 'GET',
+        'inAValidPath':     checkCachePath(url)
       };
     var failingCriteria = Object.keys(criteria).filter((value) => !criteria[value]);
     return !failingCriteria.length;
   };
 
-  var fetchType = function (request) {
+  var onInstall = function () {
+    return caches.open(cacheNames.static)
+      .then((cache) => cache.addAll(preCache));
+  };
+
+  var onActivate = function () {
+    return caches.keys()
+      .then((keys)       => Promise.all(keys
+        .filter((key)      => key.indexOf(version) !== 0)
+        .map((deleteKey)   => caches.delete(deleteKey)) )
+    );
+  };
+
+  var checkCachePath = function (url) {
+    var matchesPathPattern = cachePathPattern.exec(url.pathname);
+    var matchesPreCache = preCache.filter((path) => path === url.pathname).length;
+    return matchesPathPattern || matchesPreCache;
+  };
+
+  var resourceType = function (request) {
     let acceptHeader = request.headers.get('Accept');
     if (acceptHeader.indexOf('text/html') !== -1) {
       return 'content';
@@ -56,23 +59,21 @@
         }
       });
     });
-
   };
 
   var addToCache = function (request, response) {
     var copy = response.clone(),
-      cacheName = fetchType(request);
-    caches.open(cacheNames[cacheName])
-      .then((cache) => {
-        cache.put(request, copy);
-      });
+      cacheName = resourceType(request);
+    caches.open(cacheNames[cacheName]).then((cache) => {
+      cache.put(request, copy);
+    });
     return response;
   };
 
   var getOffline = function (request) {
-    let offlineType = fetchType(request);
+    let offlineType = resourceType(request);
     if (offlineType === 'image') {
-      return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
+      return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
     } else if (offlineType === 'content') {
       return caches.match('/offline/');
     }
@@ -81,14 +82,13 @@
 
   var findInCache = function (request) {
     return new Promise((resolve, reject) => {
-      caches.match(request)
-        .then((response) => {
-          if (response !== undefined) {
-            resolve(response);
-          } else {
-            reject();
-          }
-        });
+      caches.match(request).then((response) => {
+        if (response !== undefined) {
+          resolve(response);
+        } else {
+          reject();
+        }
+      });
     });
   };
 
@@ -96,11 +96,11 @@
     .forEach((cacheKey) => cacheNames[cacheKey] = `${version}${cacheKey}`);
 
   self.addEventListener('install', (event) => {
-    event.waitUntil(updateStaticCache().then(() => self.skipWaiting()));
+    event.waitUntil(onInstall().then(() => self.skipWaiting()));
   });
 
   self.addEventListener('activate', (event) => {
-    event.waitUntil(clearOldCaches().then(() => self.clients.claim()));
+    event.waitUntil(onActivate().then(() => self.clients.claim()));
   });
 
   self.addEventListener('message', function(event) {
@@ -112,7 +112,7 @@
 
   self.addEventListener('fetch', (event) => {
     var request = event.request,
-      requestType = fetchType(request);
+      requestType = resourceType(request);
     if (!shouldHandleFetch(request)) { return; }
 
     if (requestType === 'content') {
