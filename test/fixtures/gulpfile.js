b9e1a6fd91896768404f8525bc8c3fd5182f2131
Lyza Danger Gardner <lyza@lyza.com>
2015-12-15 15:50:22 -0800

    Adding fixture setup

---
+++ test/fixtures/gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var requireDir  = require('require-dir');
+var gulp        = require('gulp');
+var runSequence = require('run-sequence');
+
+// Require all tasks in build/tasks, including subfolders
+requireDir('../../build/tasks', { recurse: true });
+
+gulp.task('default', ['build', 'watch']);
+
+gulp.task('build', ['clean', 'publish', 'unpublish'], function (done) {
+  var tasks = [
+    'assets',
+    'content',
+    'css',
+    'rss',
+    'js'
+  ];
+
+  runSequence(tasks, function() {
+    done();
+  });
+});
--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,18 @@
 /* global before, after */
+/*eslint no-console: 0 */
 'use strict';
+var path = require('path');
+var Promise = require('bluebird');
+
+var copy   = Promise.promisify(require('fs-extra').copy);
+var rimraf = Promise.promisify(require('rimraf'));
 
 before (function () {
-  // Rimraf temp
-  // cp fixtures into temp
+  var tempDir = path.join(__dirname, 'temp'),
+    fixturesDir = path.join(__dirname, 'fixtures');
+  return rimraf(tempDir).then(function () {
+    return copy(fixturesDir, tempDir);
+  });
 });
 
 after (function () {
